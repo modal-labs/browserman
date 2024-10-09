997f2ecf7e30f1de7ddcb7820181983ee304aef3
Daniel Norberg <dano@modal.com>
2024-10-09 09:38:23 +0100

    simulate first step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import modal
 import modal.gpu
+from PIL import Image
 
 image = (
     modal.Image.debian_slim(python_version="3.10")
@@ -18,7 +19,7 @@ app = modal.App(APP_NAME, image=image)
 MODEL_NAME = "neuralmagic/Llama-3.2-11B-Vision-Instruct-FP8-dynamic"
 
 
-@app.cls(gpu=modal.gpu.H100(), container_idle_timeout=30 * 60)
+@app.cls(gpu=modal.gpu.H100(), container_idle_timeout=20 * 60)
 class Model:
     @modal.build()
     def build(self):
@@ -33,7 +34,7 @@ class Model:
         self.llm = LLM(model=MODEL_NAME, max_num_seqs=1, enforce_eager=True)
 
     @modal.method()
-    def inference(self, prompt):
+    def inference(self, prompt, image):
         from vllm import SamplingParams
 
         # Set up sampling parameters
@@ -42,9 +43,10 @@ class Model:
         # Generate the response
         inputs = {
             "prompt": prompt,
-            "multi_modal_data": {
-            },
+            "multi_modal_data": {}
         }
+        if image:
+            inputs["multi_modal_data"]["image"] = image
         outputs = self.llm.generate(inputs, sampling_params=sampling_params)
 
         return outputs[0].outputs[0].text
@@ -54,7 +56,7 @@ if __name__ == "__main__":
     prompt = """
     <|begin_of_text|><|start_header_id|>system<|end_header_id|>
     
-    You have access to the following functions:
+    You have access to the following functions to interact with web pages using a web browser:
     
     Use the function 'navigate_to' to: To open a web page
     {
@@ -69,7 +71,18 @@ if __name__ == "__main__":
         }
     }
     
-    
+    Use the function 'click_button' to: To click a button on the web page.
+    {
+        "name": "click_button",
+        "description": "Click a button on a web page in a browser",
+        "parameters": {
+            "button_text": {
+                "param_type": "string",
+                "description": "button text",
+                "required": true
+            }
+        }
+    }
     
     If a you choose to call a function ONLY reply in the following format:
     <{start_tag}={function_name}>{parameters}{end_tag}
@@ -89,11 +102,20 @@ if __name__ == "__main__":
     - Put the entire function call reply on one line
     - Always add your sources when using search results to answer the user query
     
-    You are a helpful assistant.<|eot_id|><|start_header_id|>user<|end_header_id|>
+    You are a helpful assistant. Please use a sequence of function calls to satisfy the user request.<|eot_id|><|start_header_id|>user<|end_header_id|>
     
-    To begin our journey please navigate to doordash<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+    Please order me food from doordash.<|eot_id|><|start_header_id|>system<|end_header_id|>
+    Past actions taken: 
+    * <function=navigate_to>{"url": "https://www.doordash.com/"}</function>
+    Screenshot from last action taken:<|image|>
+    
+    What action should we take using the browser with the functions provided above?<|eot_id|><|start_header_id|>assistant<|end_header_id|>
     """
 
+    # <|eot_id|><|start_header_id|>system<|end_header_id|>
+
+    image = Image.open("doordash_01.png")
+
     f = modal.Function.lookup(APP_NAME, "Model.inference")
-    result = f.remote(prompt)
+    result = f.remote(prompt, image)
     print(result)
